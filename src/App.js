56f0dac406fe0977607129d1611c69a876b6c982
ellinzhao <ellin.zhao@hotmail.com>
2017-08-11 22:38:31 -0700

    tag updating methods written

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
         super();
         this.state = {
             seenIDs: [],
-            countSeen: 0
+            countSeen: 0,
+            currID: null,
+            currAttr: [],   // [artist, movement] for simplicity's sake
+            btnVal: null
         };
     }
 
@@ -20,16 +23,48 @@ class App extends Component {
         })
     }
 
-    likeClick() {
-        this.findArt(1);
+    clickHandle(i) {
+        this.updateScores(i);
+        this.findArt();
+        // TODO: state needs to change for re-rendering... what to do with result of findArt() ?
     }
 
-    findArt(i) {
-        var maxScore = Number.MIN_SAFE_INTEGER;
+    updateScores(i) {
+        const query = firebase.database().ref("artPieces");
+        query.once("value").then(snapshot => {
+            snapshot.forEach(childSnapshot => {
+                var score = childSnapshot.child("score").val();
+                if (childSnapshot.child("artist").equals(this.state.currAttr[0])) {
+                    score += i;
+                }
+                if (childSnapshot.child("movement").equals(this.state.currAttr[1])) {
+                    score += i;
+                }
+                childSnapshot.child("score").set(score);
+            });
+        });
+    }
 
+    findArt() {
+        var maxScore = Number.MIN_SAFE_INTEGER;
+        var idToDisplay = -1;
+        var query = firebase.database().ref("artPieces");
+        query.once("value").then(snapshot => {
+            snapshot.forEach(childSnapshot => {
+                var score = childSnapshot.child("score").val();
+                var id = childSnapshot.key;
+                if (score > maxScore && this.state.seenIDs.indexOf(id) < 0) {
+                    maxScore = score;
+                    idToDisplay = id;
+                }
+            });
+        });
+        return idToDisplay;
     }
 
     render() {
+        this.updateScores(this.state.btnVal);
+        this.findArt();
         return (
             <div className="container">
                 <div className="row">
@@ -37,15 +72,10 @@ class App extends Component {
                         <Image/>
                     </div>
                     <div className="col-sm-6" id="info">
-                        <h2>Title, Artist</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum enim id tempus porta.
-                            Integer vitae rutrum arcu, quis fringilla urna. Fusce a nibh blandit, rutrum enim vitae, consectetur
-                            ligula. Duis rhoncus, enim porta tempus convallis, est tellus feugiat elit, sit amet aliquam tellus
-                            justo non odio. Donec maximus est leo, et porta dui varius et. Sed laoreet neque hendrerit mi faucibus,
-                            nec sagittis ex iaculis.Curabitur porttitor nisi quis scelerisque finibus. </p>
-                        <button type="button" className="btn btn-outline" onClick={this.likeClick}>Like</button>
-                        <button type="button" className="btn btn-outline" onClick={this.findArt(0)}>Neutral</button>
-                        <button type="button" className="btn btn-outline" onClick={this.findArt(-1)}>Dislike</button>
+                        <Info/>
+                        <button type="button" className="btn btn-outline" onClick={this.setState({btnVal: 1})}>Like</button>
+                        <button type="button" className="btn btn-outline" onClick={this.setState({btnVal: 0})}>Neutral</button>
+                        <button type="button" className="btn btn-outline" onClick={this.setState({btnVal: -1})}>Dislike</button>
                     </div>
                 </div>
             </div>
@@ -56,12 +86,18 @@ class App extends Component {
 class Image extends Component {
     render() {
         return (<h3>image</h3>)
+        // should get attr as key-vals from App
     }
 }
 
 class Info extends Component {
     render() {
-        return (<h3>info</h3>)
+        return (
+            <div>
+                <h2 id="infoHeader">Title, Artist</h2>
+                <p id="infoText"></p>
+            </div>
+        )
     }
 }
 
